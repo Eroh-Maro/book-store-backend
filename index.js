3e7ed7902cc048f26291a080cdacb122ee30ac06
Eroh Oghenemaro Divine <maroeroh@gmail.com>
2025-06-20 12:35:33 +0100

    restore to default fore debug

--- index.js
+++ index.js
@@ -1,43 +1,48 @@
 import express from "express";
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import bookRoutes from "./src/books/book.route.js";
-import cors from "cors";
 import orderRoutes from "./src/orders/order.route.js";
 import userRoutes from "./src/users/user.route.js";
 import adminRoutes from "./src/stats/admin.stats.js";
 
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
 app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://book-store-frontend-hgdn.vercel.app/",
       "http://localhost:5173",
+      "https://book-store-frontend-hgdn.vercel.app/"
     ],
     credentials: true,
   })
 );
 
+// routes
 app.use("/api/books", bookRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/auth", userRoutes);
 app.use("/api/admin", adminRoutes);
 
-// Register the root route outside of main
+// root route
+app.get("/", (req, res) => {
+  res.send("Book Store Server is running!");
+});
+
 async function main() {
-    await mongoose.connect(process.env.DB_URL)
-    app.use('/', (req, res) => {
-        res.send('hello world')
-       })
-} 
+  await mongoose.connect(process.env.DB_URL);
+}
+
 main()
-  .then(() => console.log("MongoDB connected succesfully!"))
+  .then(() => console.log("Mongodb connect successfully!"))
   .catch((err) => console.log(err));
 
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
